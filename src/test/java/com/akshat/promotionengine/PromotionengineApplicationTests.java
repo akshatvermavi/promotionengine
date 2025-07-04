4511f60217c5578950bb50b0a07f94a26e94ad1f
akshatvermavi <akshatvermanike@gmail.com>
2025-07-05 00:18:45 +0530

    project setup successfully

---
+++ src/test/java/com/akshat/promotionengine/PromotionengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshat.promotionengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
